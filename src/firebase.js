9d313c254b97a74e64ebcffe7e160edcaa06dd6e
DASH SANTOSH <santdas36@gmail.com>
2021-05-25 12:31:27 +0530

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+console.log(firebaseConfig);
 
 const firebaseAdmin = firebase.initializeApp(firebaseConfig);
 const provider = new firebase.auth.GoogleAuthProvider();
