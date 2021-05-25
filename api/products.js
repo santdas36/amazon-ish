812e0ce6f7a17f0c266af62cd44ed6878458aa7d
DASH SANTOSH <santdas36@gmail.com>
2021-05-25 21:56:51 +0530

    Products api endpoint

---
+++ api/products.js
@@ -0,0 +1,22 @@
+const admin = require('firebase-admin');
+const firebase = !admin.apps.length ? admin.initializeApp({
+	credential: admin.credential.cert(JSON.parse(new Buffer(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64').toString('utf-8'))),
+}) : admin.app();
+
+const db = firebase.firestore();
+
+export default async (request, response) => {
+	
+	if (request.method !== 'GET') {
+		return response.status(400).send('400 Bad Request');
+	}
+	
+	try {
+		const products = db.collection("products").get();
+		return response.status(200).json(products);
+	}
+	catch(e) {
+		console.log(e);
+		return response.status(500).send('Internal Error');
+		}
+}
